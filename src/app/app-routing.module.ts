8a04564a27940158a881cc11d754586598dfa3fb
TatuWasca <pablorubiolo2004@gmail.com>
2022-08-28 18:16:35 -0300

    FrontEnd terminado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AppComponent } from './app.component';
 import { Pagina404Component } from './componentes/pagina404/pagina404.component';
 import { InicioComponent } from './componentes/inicio/inicio.component';
-import { TokenService } from './service/token.service';
-import { CargandoComponent } from './componentes/cargando/cargando.component';
 
 const routes: Routes = [
   {path: 'inicio', component:InicioComponent},
   {path: 'error404', component: Pagina404Component},
-  {path: 'cargando', component: CargandoComponent},
   {path: '', pathMatch: 'full', redirectTo: 'inicio'},
   {path: '**', redirectTo:"error404"}
 ];
